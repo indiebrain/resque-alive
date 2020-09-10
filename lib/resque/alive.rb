1f813f6562463972ed631fa577d92c23b6f48d95
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-09-10 18:12:11 -0400

    Initial commit

---
+++ lib/resque/alive.rb
@@ -0,0 +1,8 @@
+require "resque/alive/version"
+
+module Resque
+  module Alive
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/resque/alive/version.rb
@@ -0,0 +1,5 @@
+module Resque
+  module Alive
+    VERSION = "0.1.0"
+  end
+end
